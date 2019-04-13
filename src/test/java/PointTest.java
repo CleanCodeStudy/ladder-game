65d9d5d3e664191609d4dd3f9b45d4ede41f5838
최유성 <cys2272@woowahan.com>
2019-04-14 01:04:39 +0900

    ladder-game init

---
+++ src/test/java/PointTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void 오른쪽_연결된_포인트_생성하기() {
+        assertEquals(Point.createRightLinkedPoint(1).getLinkedType(),LinkedType.RIGHT);
+    }
+
+    @Test
+    public void 왼쪽_연결된_포인트_생성하기() {
+        assertEquals(Point.createLeftLinkedPoint(2).getLinkedType(),LinkedType.LEFT);
+    }
+
+    @Test
+    public void 연결안된_포인트_생성하기() {
+        assertEquals(Point.createNotLinkedPoint(1).getLinkedType(),LinkedType.NONE);
+    }
+}
